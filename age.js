bdc45321589721547e86dd81b0d2ba39c7bd1b9a
Maryam Shah <eng.maryamshah@gmail.com>
2025-09-05 21:03:10 +0500

    age calculator

---
+++ age.js
@@ -0,0 +1,21 @@
+function calculateAge(){
+    let dob=new Date(document.getElementById("dob").value);
+    if(isNaN(dob))return;
+    let now =new Date();
+    let years = now.getFullYear() -dob.getFullYear();
+    let months=now.getMonth()- dob.getMonth();
+    let days=now.getDate() - dob.getDate();
+    if(days<0){
+        months--;
+ days += new Date(now.getFullYear(), now.getMonth(), 0).getDate();
+
+
+    }
+    if (months<0){
+        years--;
+        months+=12;
+    }
+    document.getElementById("result").textContent=
+    `Result: You are ${years} years, ${months} months, and ${days} old.`;
+
+}
